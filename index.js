f229d12070b997dd71043d2b5f36dc6448134358
Crisbel <crisbel25c@gmail.com>
2025-11-18 18:37:23 -0400

    prueba erronea

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function suma(a,b) {
-  return a + b
+function suma(a, b) {
+  return a + b;
 }
 
 module.exports = suma;
--- tests/suma.test.js
+++ tests/suma.test.js
@@ -1,5 +1,5 @@
 const suma = require("../index");
 
 test("suma 2 + 3 = 5", () => {
-  expect(suma(2, 3)).toBe(5);
+  expect(suma(2, 3)).toBe(6);
 })
